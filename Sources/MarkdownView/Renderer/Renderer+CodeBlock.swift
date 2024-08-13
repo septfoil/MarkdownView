e33c710f81b7dcfee896d6813e4b43c2fb7b15a5
Fei Ye <septfoil@hotmail.com>
2024-08-13 09:41:24 +0800

    Update Renderer+CodeBlock.swift

--- Sources/MarkdownView/Renderer/Renderer+CodeBlock.swift
+++ Sources/MarkdownView/Renderer/Renderer+CodeBlock.swift
@@ -5,6 +5,7 @@ import SwiftUI
 extension Renderer {
     mutating func visitInlineCode(_ inlineCode: InlineCode) -> Result {
         var attributedString = AttributedString(stringLiteral: inlineCode.code)
+        attributedString.font = configuration.fontGroup.body.monospaced()
         attributedString.foregroundColor = configuration.inlineCodeTintColor
         attributedString.backgroundColor = configuration.inlineCodeTintColor.opacity(0.1)
         return Result(SwiftUI.Text(attributedString))
